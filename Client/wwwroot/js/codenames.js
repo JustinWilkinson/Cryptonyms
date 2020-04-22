5fb05eca9fc3eeb2dc2a6b3019ad0f755c04fc10
Justin Wilkinson <justin_wilkinson@talktalk.net>
2020-04-22 16:19:54 +0100

    Accelerate animation

--- Client/wwwroot/js/codenames.js
+++ Client/wwwroot/js/codenames.js
@@ -60,20 +60,20 @@
 
         if (showAssassin) {
             $('#Balloons').hide();
-            $('.animation-container').prepend('<img id="Assassin" class="img-endgame img-assassin" src="/images/Assassin.png" width="0" height="0" />');
+            $('.animation-container').prepend('<img id="Assassin" class="img-endgame img-assassin" src="/images/assassin.png" width="0" height="0" />');
 
             let spyColour = winningTeam === 'blue' ? 'red' : 'blue';
             $('.animation-container').prepend(`<img id="Spy" class="img-endgame img-spy" src="/images/${spyColour}-man.png" width="0" height="0" />`);
             $('.animation-container').prepend('<img id="Bullet" class="img-endgame img-bullet" src="/images/bullet.png" width="10" height="5" />')
 
             masterTimeline.add(gsap.timeline().to('#Spy', {
-                duration: 1,
+                duration: 0.8,
                 width: '20vh',
                 height: '40vh',
                 opacity: 1,
             }));
             masterTimeline.add(gsap.timeline().to('#Assassin', {
-                duration: 2,
+                duration: 1.6,
                 width: '20vh',
                 height: '40vh',
                 rotationY: 360,
@@ -81,21 +81,21 @@
                 onComplete: () => $('#Bullet').css('opacity', 1)
             }));
             masterTimeline.add(gsap.timeline().to('#Bullet', {
-                duration: 0.4,
+                duration: 0.3,
                 opacity: 1,
                 x: -Math.max(document.documentElement.clientWidth, window.innerWidth || 0) / 2.6,
                 onComplete: () => setTimeout(() => $('#Bullet').remove(), 50)
             }));
             masterTimeline.add(gsap.timeline().to('#Spy', {
                 delay: 0.1,
-                duration: 0.5,
+                duration: 0.4,
                 width: '20vh',
                 height: '40vh',
                 opacity: 0
             }));
             masterTimeline.add(gsap.timeline().to('#Assassin', {
-                delay: 1,
-                duration: 1,
+                delay: 0.25,
+                duration: 0.6,
                 opacity: 0,
                 onComplete: () => {
                     $('#Assassin').remove();
