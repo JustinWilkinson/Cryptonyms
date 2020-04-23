fd783a3ccb4cdd2e3357c007c1280a7019f48e6f
Justin Wilkinson <justin_wilkinson@talktalk.net>
2020-04-23 16:22:01 +0100

    Include messages in clean up job

--- Server/Repository/MessageRepository.cs
+++ Server/Repository/MessageRepository.cs
@@ -4,14 +4,17 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Linq;
 
 namespace Cryptonyms.Server.Repository
 {
     public interface IMessageRepository
     {
-        void AddMessage(string MessageBoardId, GameMessage message);
+        void AddMessage(string messageBoardId, GameMessage message);
 
         IEnumerable<GameMessage> GetGameMessagesForGroup(string chatId);
+
+        void DeleteMessagesForGames(IEnumerable<Guid> gameIds);
     }
 
     public class MessageRepository : Repository, IMessageRepository
@@ -50,6 +53,41 @@ namespace Cryptonyms.Server.Repository
             catch (Exception ex)
             {
                 _logger.LogError(ex, $"An error occurred retrieving messages for board '{messageBoardId}'.");
+                throw;
+            }
+        }
+
+
+        public void DeleteMessagesForGames(IEnumerable<Guid> gameIds)
+        {
+            try
+            {
+                var gameIdsAsStrings = gameIds.Select(x => x.ToString()).ToList();
+                var messageBoardIdsToDelete = new List<string>();
+                foreach (var messageBoardId in Execute("SELECT DISTINCT MessageBoardId FROM Messages", x => x.ToString()))
+                {
+                    foreach (var gameId in gameIdsAsStrings)
+                    {
+                        if (messageBoardId.StartsWith(gameId))
+                        {
+                            messageBoardIdsToDelete.Add(messageBoardId);
+                        }
+                    }
+                }
+
+                ExecuteInTransaction((connection) =>
+                {
+                    foreach (var messageBoardId in messageBoardIdsToDelete)
+                    {
+                        var command = new SQLiteCommand("DELETE FROM Messages WHERE MessageBoardId = @MessageBoardId", connection);
+                        command.AddParameter("@MessageBoardId", messageBoardId);
+                        command.ExecuteNonQuery();
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An error occurred clearing messages for old games.");
                 throw;
             }
         }
